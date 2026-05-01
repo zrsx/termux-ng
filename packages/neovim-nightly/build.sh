da03f3bb93a95c0a299d637cbf4fd596d11b089a
pkgitbot <pkgitbot@users.noreply.github.com>
2026-05-01 08:34:16 +0000

    bump(main/neovim-nightly): 0.13.0~dev-386+g858cd3d106
    This commit has been automatically submitted by Github Actions.

--- packages/neovim-nightly/build.sh
+++ packages/neovim-nightly/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Ambitious Vim-fork focused on extensibility and agility 
 TERMUX_PKG_LICENSE="Apache-2.0, VIM License"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
-TERMUX_PKG_VERSION="0.13.0~dev-375+g1e06e95662"
+TERMUX_PKG_VERSION="0.13.0~dev-386+g858cd3d106"
 TERMUX_PKG_SRCURL="https://github.com/neovim/neovim/archive/${TERMUX_PKG_VERSION##*+g}.tar.gz"
-TERMUX_PKG_SHA256=87dd67c4bb35fcb106284600abdb8639685df3f2ebef9b2bb87ef1e5d8ed3f44
+TERMUX_PKG_SHA256=938bc667caa90821b3df297b113e6edf642ed8b91152eaec1729c52786250833
 TERMUX_PKG_REPOLOGY_METADATA_VERSION="${TERMUX_PKG_VERSION%%~*}"
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libmsgpack, libunibilium, libuv, libvterm (>= 1:0.3-0), lua51-lpeg, luajit, luv, tree-sitter, tree-sitter-parsers, utf8proc"
 TERMUX_PKG_BREAKS="neovim"
