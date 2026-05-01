b20b96dc219b1e8d7f51934c8bb71db494f05991
GEORGE <201302550+Aasyaco@users.noreply.github.com>
2026-05-02 01:16:58 +0600

    termux: copy full prefix into massagedir instead of incremental copy

--- scripts/build/termux_step_copy_into_massagedir.sh
+++ scripts/build/termux_step_copy_into_massagedir.sh
@@ -1,7 +1,10 @@
 termux_step_copy_into_massagedir() {
-	local DEST="$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX_CLASSICAL"
-	mkdir -p "$DEST"
-	# Copy files changed during the build into massagedir in order to massage them
-	tar -C "$TERMUX_PREFIX_CLASSICAL" -N "$TERMUX_BUILD_TS_FILE" --exclude='tmp' --exclude='__pycache__' -cf - . | \
-		tar -C "$DEST" -xf -
+    local DEST="$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX_CLASSICAL"
+    mkdir -p "$DEST"
+    echo "$TERMUX_PREFIX_CLASSICAL"
+    ls -1 "$TERMUX_PREFIX_CLASSICAL"
+    tar -C "$TERMUX_PREFIX_CLASSICAL" \
+        --exclude='tmp' \
+        --exclude='__pycache__' \
+        -cf - . | tar -C "$DEST" -xf -
 }
