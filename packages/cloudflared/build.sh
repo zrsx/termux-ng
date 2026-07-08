4f0e4d043d5f8b703a7fc023ae67b13fe9a8d5fe
pkgitbot <pkgitbot@users.noreply.github.com>
2026-07-08 14:29:28 +0000

    bump(main/cloudflared): 2026.7.0
    This commit has been automatically submitted by Github Actions.

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2026.6.1"
+TERMUX_PKG_VERSION="2026.7.0"
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c3619a29cc529a5bc43ba99e1b298483ac0b8d8c412f69575c089a6bd4a7867d
+TERMUX_PKG_SHA256=f986f5b2a7309bfe21db8d694bdf09ae2904b0674c67744aa645038e15608383
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
